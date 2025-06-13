5ce78d8aa84b4da4967fa3e635a3ef08165388c2
ilyesbar <84916577+ilyesbar@users.noreply.github.com>
2025-06-13 12:39:57 +0100

    Update carousel_prod.php

--- structure/carousel_prod.php
+++ structure/carousel_prod.php
@@ -6,7 +6,7 @@ border: 2px solid var(--tertiary);}
     .gallery-container img{object-fit:contain;}
 </style>
 <section class="section padding-global padding-section-small">
-    <h2>Notre liste de Produits</h2>
+    <h2>Notre liste de  Produits</h2>
     <div class="home-v2-overlay-style"></div>
     <div data-w-id="4723f713-5fee-203c-6121-c890fa39a73c" class="home-v2-carousel-wrapper">
         <div style="transform: translate3d(-1183.46px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; will-change: transform;"
